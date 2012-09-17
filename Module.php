a9b52a249d7e2d05058592980a50e0140aa2407d
Roberto Wesley Overdijk <wesley@ratus.nl>
2012-09-17 16:40:19 +0200

    Cleanup

--- Module.php
+++ Module.php
@@ -1,45 +1,33 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
-namespace ZendSkeletonModule;
+namespace SxBootstrap;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Mvc\ModuleRouteListener;
-
-class Module implements AutoloaderProviderInterface
+class Module
 {
-    public function getAutoloaderConfig()
+    public function getServiceConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-		    // if we're in a namespace deeper than one level we need to fix the \ in the path
-                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
+            'asset_manager' => array(
+                'resolver_configs' => array(
+                    'collections' => array(
+                        'js/bootstrap.js' => array(
+                            'js/bootstrap-affix.js',
+                            'js/bootstrap-alert.js',
+                            'js/bootstrap-button.js',
+                            'js/bootstrap-carousel.js',
+                            'js/bootstrap-collapse.js',
+                        ),
+                    ),
+
+                    'map' => array(
+                        'js/bootstrap-affix.js' => 'vendor/twitter/bootstrap/js/bootstrap-affix.js',
+                        'js/bootstrap-alert.js' => 'vendor/twitter/bootstrap/js/bootstrap-alert.js',
+                        'js/bootstrap-button.js' => 'vendor/twitter/bootstrap/js/bootstrap-button.js',
+                        'js/bootstrap-carousel.js' => 'vendor/twitter/bootstrap/js/bootstrap-carousel.js',
+                        'js/bootstrap-collapse.js' =>'vendor/twitter/bootstrap/js/bootstrap-collapse.js',
+                    ),
                 ),
             ),
         );
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function onBootstrap($e)
-    {
-        // You may not need to do this if you're doing it elsewhere in your
-        // application
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Sample, skeleton module for use with the ZF2 MVC layer.
+A simple module that depends on [AssetManager](http://github.com/RWOverdijk/AssetManager),
+including Twitter Bootstrap allowing you to build custom versions when you override the configuration.
+
+This module is very basic, and only supplies twitter bootstrap.
